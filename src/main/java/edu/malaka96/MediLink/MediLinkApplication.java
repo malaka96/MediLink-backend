44e04ef62fe213909686041745a9955a6b1ca94d
malaka96 <madhubhashana655@gmail.com>
2026-03-18 23:41:08 +0530

    initial commit

---
+++ src/main/java/edu/malaka96/MediLink/MediLinkApplication.java
@@ -0,0 +1,13 @@
+package edu.malaka96.MediLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediLinkApplication.class, args);
+	}
+
+}
